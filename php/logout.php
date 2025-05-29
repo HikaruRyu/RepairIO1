c40dac5d8937572591821870c8fd67933f36bcd5
Judit <juditclosa@gmail.com>
2025-05-29 21:47:48 +0200

    Die cyka

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'db.php';
-include_once("headers.php");
-
-setcookie('user', '', time() - 3600, '/');
-
-header('/');
-exit;
--- src/js/client.js
+++
@@ -1,2 +0,0 @@
-// src/js/client.js
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
